0a93fe9bc92ea1350fd6d84b77d6f73cf50c56a1
Sean Morgan <insideseanshead@gmail.com>
2020-10-29 22:25:56 -0700

    created get routes for collections

--- server.js
+++ server.js
@@ -16,3 +16,38 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+
+db.Workout.create({ name: "Test Case" })
+  .then(dbworkout => {
+    console.log(dbworkout);
+  })
+  .catch(({ message }) => {
+    console.log(message);
+  });
+
+  app.get("/workout", (req, res) => {
+    db.Workout.find({})
+      .then(dbworkout => {
+        res.json(dbworkout);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+  
+  app.get("/exercise", (req, res) => {
+    db.Exercise.find({})
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.json(err);
+      });
+  });
+
+  
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
